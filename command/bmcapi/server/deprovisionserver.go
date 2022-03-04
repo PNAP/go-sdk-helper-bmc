33c67a0c26c50d4eccc57303dcea11aebf86c994
pavlej <pavlej@NSDT-PAVLEJ.ccbill-hq.local>
2022-03-04 21:55:57 +0100

    deprovision server command added

---
+++ command/bmcapi/server/deprovisionserver.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/PNAP/go-sdk-helper-bmc/dto"
+
+	//"net/http"
+	"context"
+
+	"github.com/PNAP/go-sdk-helper-bmc/receiver"
+	bmcapiclient "github.com/phoenixnap/go-sdk-bmc/bmcapi"
+)
+
+// DeprovisionServerCommand represents command that deprovisions specific server
+type DeprovisionServerCommand struct {
+	receiver          receiver.BMCSDK
+	serverID          string
+	relinquishIpBlock bmcapiclient.RelinquishIpBlock
+}
+
+// Execute deprovisions specific server
+func (command *DeprovisionServerCommand) Execute() (*string, error) {
+
+	result, httpResponse, err := command.receiver.APIClient.ServersApi.ServersServerIdActionsDeprovisionPost(context.Background(), command.serverID).RelinquishIpBlock(command.relinquishIpBlock).Execute()
+
+	errResolver := dto.NewErrorResolver(httpResponse, err)
+
+	if errResolver.Error == nil {
+		return &result, nil
+	}
+	return nil, fmt.Errorf("DeprovisionServerCommand %s", errResolver.Error)
+}
+
+//NewDeprovisionServerCommand constructs new commmand of this type
+func NewDeprovisionServerCommand(receiver receiver.BMCSDK, serverID string, relinquishIpBlock bmcapiclient.RelinquishIpBlock) *DeprovisionServerCommand {
+
+	return &DeprovisionServerCommand{receiver, serverID, relinquishIpBlock}
+}
