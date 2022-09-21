eb8098167f010419a7e6c687f67ab947223bea40
nikolav <nikolav@NSDT-NIKOLAV.ccbill-hq.local>
2022-09-21 17:41:01 +0200

    Added Network Storage API commands

---
+++ command/networkstorageapi/storagenetwork/getstoragenetworkvolume.go
@@ -0,0 +1,36 @@
+package storagenetwork
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/PNAP/go-sdk-helper-bmc/dto"
+	"github.com/PNAP/go-sdk-helper-bmc/receiver"
+	networkstorageapiclient "github.com/phoenixnap/go-sdk-bmc/networkstorageapi"
+)
+
+// GetStorageNetworkVolumeCommand represents command that retrieves details about specific volume on a storage network on the account
+type GetStorageNetworkVolumeCommand struct {
+	receiver         receiver.BMCSDK
+	storageNetworkId string
+	volumeId         string
+}
+
+// Execute runs GetStorageNetworkVolumeCommand
+func (command *GetStorageNetworkVolumeCommand) Execute() (*networkstorageapiclient.Volume, error) {
+
+	volume, httpResponse, err := command.receiver.NetworkStorageAPIClient.StorageNetworksApi.StorageNetworksStorageNetworkIdVolumesVolumeIdGet(context.Background(), command.storageNetworkId, command.volumeId).Execute()
+
+	errResolver := dto.NewErrorResolver(httpResponse, err)
+
+	if errResolver.Error == nil {
+		return volume, nil
+	}
+	return nil, fmt.Errorf("GetStorageNetworkVolumeCommand %s", errResolver.Error)
+}
+
+//NewGetStorageNetworkVolumeCommand constructs new commmand of this type
+func NewGetStorageNetworkVolumeCommand(receiver receiver.BMCSDK, storageNetworkId string, volumeId string) *GetStorageNetworkVolumeCommand {
+
+	return &GetStorageNetworkVolumeCommand{receiver, storageNetworkId, volumeId}
+}
